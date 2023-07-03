1db0ac320078652e1290a0dc67060c758ff6c9e7
miladesmailpour <milad.esmailpour@gmail.com>
2023-07-03 16:08:01 -0400

    updateThought added to the tought controller for updating a thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -60,6 +60,22 @@ module.exports = {
         catch (err) {
             res.status(500).json(err);
         }
+    },
+    
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            if (!thought) {
+                return res.status(404).json({ message: `Thought with the ID ${req.params.thoughtId} NOT found` });
+            }
+            res.json(thought);
+        } 
+        catch (err) {
+            res.status(500).json(err);
+        }
     }
-
-    };
+};

b20b60b9ab8eb0ebd81e530e34cbc9b83a3c5a26
miladesmailpour <milad.esmailpour@gmail.com>
2023-07-03 15:08:12 -0400

    Thought model and its reaction added

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const { format_date } = require('../utils/helpers');
+const reactionSchema = require('./reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => format_date(timestamp),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+})
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
